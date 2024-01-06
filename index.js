ec9a620fa305e9cac4acd4b7835ea79953765382
AkashSonone <121234041+AkashSonone@users.noreply.github.com>
2024-01-06 11:14:56 +0530

    Create index.js
    This is a JavaScript file

---
+++ index.js
@@ -0,0 +1,49 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9e5ccc7a58mshe3515001a8a5c38p19028bjsn984925f11161',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '9e5ccc7a58mshe3515001a8a5c38p19028bjsn984925f11161',
+// 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle',options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error))
+
+
+//     const url = 'https://forecast9.p.rapidapi.com/';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '9e5ccc7a58mshe3515001a8a5c38p19028bjsn984925f11161',
+// 		'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
